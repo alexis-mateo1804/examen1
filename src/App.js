4b5725b25616d68264596c499d9897b3c9f99aa1
alexis mateo <boku-heromateo@hotmail.com>
2021-12-20 22:46:04 -0600

    fox2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-undef */
 import logo from './logo.svg';
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 import axios from "axios"
@@ -18,33 +21,55 @@ export async function envAPI(url) {
 }
 
 function App() {
+  const [dataRick, setDataRick] = useState(null);
+  const [personajes, setPersonajes] = useState([]);
+
+  // eslint-disable-next-line no-unused-vars
+  const response = async (name) => envAPI(name);
   useEffect(() => {
     async function loadapi() {
-        const resp = await Response("all");
+        const resp = await response("character");
         const data = resp?.data;
-        HandleApiData (data);
+        setDataRick(data);   
     }
     loadapi();
     return function cleanup() {
         loadapi();
     }
   }, []);
+
+  useEffect(() => {
+    console.log(dataRick);
+    if (dataRick !== null) {
+      setPersonajes(dataRick.results);
+      console.log(dataRick?.results[0].image);
+      console.log(personajes);
+    }
+  }, [dataRick]);
+
+  useEffect(() => {
+    console.log(personajes);
+  }, [personajes]);
+
+  const valor = 5;
   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {personajes.length !== 0 && 
+          personajes.map((item, index) => {
+            console.log(item)
+            return (
+              <div key={index}>
+                <img src={item.image} className="App-logo" alt="logo" />
+                <p>{item.name}</p>
+                <p>Especie: {item.species}</p>
+              </div>
+              
+            );
+          })}
+
       </header>
     </div>
   );
